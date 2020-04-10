7e3b1ac11676e216e2d83aadee61eb2088d2d004
Vlad Danilov <danilovxp@gmail.com>
2020-04-09 20:37:43 -0700

    Update Beginner Series #2 Clock.js

--- kyu8/Beginner Series #2 Clock.js
+++ kyu8/Beginner Series #2 Clock.js
@@ -1,12 +1,10 @@
 /* 
-
-<b>https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a</b>
+https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
 Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
 Your task is to make 'Past' function which returns time converted to milliseconds.
 Example:
 past(0, 1, 1) == 61000
 Input constraints: 0 <= h <= 23, 0 <= m <= 59, 0 <= s <= 59
-
 */
 
 
