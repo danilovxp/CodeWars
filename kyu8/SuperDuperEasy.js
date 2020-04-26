9919d7aaedea5a3139706129c758ddedbdfa41ba
Vlad Danilov <danilovxp@gmail.com>
2020-04-25 20:29:04 -0700

    Multiply

---
+++ kyu8/SuperDuperEasy.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55a5bfaa756cfede78000026
+
+// Super Duper Easy
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+  return typeof x === "number" ? x * 50 + 6 : "Error";
+}
