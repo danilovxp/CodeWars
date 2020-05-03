2be193ff532bc34aae797bb7e1786a3458e9defe
Vlad Danilov <danilovxp@gmail.com>
2020-05-02 20:00:30 -0700

    Grasshopper - Messi Goals

---
+++ kyu8/NumToStr.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions/javascript
+
+// Convert a Number to a String!
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// numberToString(123); // returns '123';`
+// numberToString(999); // returns '999';`
+
+function numberToString(num) {
+  return String(num);
+}
