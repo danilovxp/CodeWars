fa8cf4d76ffe584cd6e972caeb87a0aa0425f9ca
Vlad Danilov <danilovxp@gmail.com>
2020-05-04 19:38:35 -0700

    Shoping List

---
+++ kyu8/GrasshopperShoppingList.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/560c31275c39c481c4000022
+
+// Grasshopper - Shopping list
+
+// Ordering food
+// You are in charge of ordering food for a party. You are going to need 4 sandwiches, 6 salads, 5 wraps, and 10 orders of french fries. The cost per item of food is:
+
+// food	price
+// sandwich	$8.00
+// salad	$7.00
+// wrap	$6.50
+// french fries	$1.20
+// Create 4 variables to store the quantity of each type of food with the following names:
+
+// sandwiches
+// salads
+// wraps
+// frenchFries
+// Create a variable named totalPrice that finds the cost of all of the food.
+
+let sandwiches = 4;
+let salads = 6;
+let wraps = 5;
+let frenchFries = 10;
+let totalPrice = sandwiches * 8 + salads * 7 + wraps * 6.5 + frenchFries * 1.2;
