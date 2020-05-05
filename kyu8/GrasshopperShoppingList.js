d25e267e8b299def9ab6602a01cd0358f5a9f6e4
Vlad Danilov <danilovxp@gmail.com>
2020-05-04 19:44:48 -0700

    Shopping list

--- kyu8/GrasshopperShoppingList.js
+++ kyu8/GrasshopperShoppingList.js
@@ -1,6 +1,7 @@
 // https://www.codewars.com/kata/560c31275c39c481c4000022
 
 // Grasshopper - Shopping list
+// Grasshopper - Shopping list
 
 // Ordering food
 // You are in charge of ordering food for a party. You are going to need 4 sandwiches, 6 salads, 5 wraps, and 10 orders of french fries. The cost per item of food is:
