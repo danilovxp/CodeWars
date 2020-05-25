e9208c619ebe7eb4252798dab5f5530080532c6a
Vlad Danilov <danilovxp@gmail.com>
2020-05-24 20:37:28 -0700

    Convert a String to a Number!

---
+++ kyu8/ConvertStringToNumber.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+// Convert a String to a Number!
+
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+var stringToNumber = function(str) {
+  return +str;
+};
