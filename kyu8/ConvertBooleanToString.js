617764f0fa8cfacfbb6596cb271fc08426bbe141
Vlad Danilov <danilovxp@gmail.com>
2020-05-25 20:29:45 -0700

    Convert a Boolean to a String

---
+++ kyu8/ConvertBooleanToString.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+// Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b) {
+  return String(b);
+}
