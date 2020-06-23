06cd15d0bcd2f64b3b06094b658ec8184e0354fc
Vlad Danilov <danilovxp@gmail.com>
2020-06-22 19:35:44 -0700

    Convert to Binary

---
+++ kyu8/ConvertToBinary.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/59fca81a5712f9fa4700159a
+
+// Convert to Binary
+
+// Task Overview
+// Given a non-negative integer n, write a function toBinary/ToBinary which returns that number in a binary format.
+
+function toBinary(n) {
+  return +n.toString(2);
+}
