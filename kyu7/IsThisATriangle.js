747053daab7167028969256f46540780f26c5ecd
Vlad Danilov <danilovxp@gmail.com>
2020-07-16 20:55:42 -0700

    Is this a triangle?

---
+++ kyu7/IsThisATriangle.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/56606694ec01347ce800001b
+
+// Is this a triangle?
+
+// Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+const isTriangle = (a, b, c) =>
+  a + b > c && b + c > a && c + a > b ? true : false;
