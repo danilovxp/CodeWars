2739078cf778988bae84d255b714d0274b8d85ac
Vlad Danilov <danilovxp@gmail.com>
2020-08-13 21:46:06 -0700

    You Can't Code Under Pressure #1

---
+++ kyu8/YouCantCodeUnderPressure1.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+// You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+const doubleInteger = (i) => i * 2;
