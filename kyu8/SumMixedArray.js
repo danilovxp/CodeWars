c78f97f67d1391dbcdc33e94b674e5353f5e2817
Vlad Danilov <danilovxp@gmail.com>
2020-09-18 22:12:15 -0700

    Sum Mixed Array

---
+++ kyu8/SumMixedArray.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009
+
+// Sum Mixed Array
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+const sumMix = (x) => x.reduce((a, c) => a + +c, 0);
