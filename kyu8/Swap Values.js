87c224b6e98ac6610e4ca1d89c3c1b9fc236f96c
danilovxp <danilovxp@gmail.com>
2020-12-14 17:51:54 -0800

    Swap Values

---
+++ kyu8/Swap Values.js
@@ -0,0 +1,10 @@
+//* Swap Values
+
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+const swapValues = (arr) => arr.reverse();
+
+console.log([0, 1]);
+console.log([1, 0]);
