beff3e97aeced5c17ee421f757befedb10b49f81
danilovxp <danilovxp@gmail.com>
2020-12-17 15:53:02 -0800

    Mumbling

---
+++ kyu7/Mumbling.js
@@ -0,0 +1,31 @@
+// * Mumbling
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+  let result = '';
+	for(let i = 0; i < s.length; i++){
+    result += 
+    s[i].toUpperCase() + 
+    s[i].toLowerCase().repeat(i) + 
+    (s.length - 1 !== i ? '-' : '')
+  }
+  return result
+}
+
+
+console.log(accum("ZpglnRxqenU"))
+//, "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+  console.log(accum("NyffsGeyylB"))
+  //, "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+  console.log(accum("MjtkuBovqrU"))
+  //, "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+  console.log(accum("EvidjUnokmM"))
+  //, "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+  console.log(accum("HbideVbxncC"))
+  //, "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
