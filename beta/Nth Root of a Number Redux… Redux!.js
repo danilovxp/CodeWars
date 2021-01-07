5efe42f0614deabdc17b0ac0f97a96edc823f3bb
danilovxp <danilovxp@gmail.com>
2021-01-07 08:49:58 -0800

    little cahnges

--- beta/Nth Root of a Number Redux… Redux!.js
+++ beta/Nth Root of a Number Redux… Redux!.js
@@ -3,9 +3,9 @@
 //   Task
 // Given two numbers x and n, calculate the nth root of x, e.g. r such that r^n = x. However, your using a legacy library that doesn't contain several useful functions, see below.
 
-const root = (x, nth) =>  x ** (1/nth)
+const root = (x, nth) => x ** (1 / nth);
 
-console.log(root(4, 2))// // 2
-console.log(root(8, 3))// // 2
-console.log(root(256, 4))// // 4
-console.log(root(9, 2))// // 3
+console.log(root(4, 2)); //  2
+console.log(root(8, 3)); //  2
+console.log(root(256, 4)); // 4
+console.log(root(9, 2)); // 3
