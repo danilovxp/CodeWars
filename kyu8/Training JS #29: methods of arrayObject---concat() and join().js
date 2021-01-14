7f492f33d5ce9e066d7ba3c68dfafd4490c16b16
danilovxp <danilovxp@gmail.com>
2021-01-14 12:12:00 -0800

    message

--- kyu8/Training JS #29: methods of arrayObject---concat() and join().js
+++ kyu8/Training JS #29: methods of arrayObject---concat() and join().js
@@ -10,14 +10,25 @@
 
 // Don't complain about the situation like 1>1 is not reasonable, it is just a separator.
 
-function bigToSmall(arr){
-  let res = []
-  for(let i = 0; i < arr.length; i++){
-    res = res.concat(arr[i])
+function bigToSmall(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    res = res.concat(arr[i]);
   }
-  return res.sort((a, b) => b - a).join(">")
+  return res.sort((a, b) => b - a).join(">");
 }
 
-console.log(bigToSmall([[1,2],[3,4],[5,6]]))// , "6>5>4>3>2>1")
-console.log(bigToSmall([[1,3,5],[2,4,6]]))// , "6>5>4>3>2>1");
-console.log(bigToSmall([[1,1],[1],[1,1]]))// , "1>1>1>1>1");
+console.log(
+  bigToSmall([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ])
+); // , "6>5>4>3>2>1")
+console.log(
+  bigToSmall([
+    [1, 3, 5],
+    [2, 4, 6],
+  ])
+); // , "6>5>4>3>2>1");
+console.log(bigToSmall([[1, 1], [1], [1, 1]])); // , "1>1>1>1>1");
