6688062e857c04d927803bfbca92cccd1538082b
danilovxp <danilovxp@gmail.com>
2021-02-05 14:05:00 -0800

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('object')
+'use strict'
+const f = (b) => {
+  var c = 15;
+  console.log('f is', b);
+}
+
+
+function f2(b) {
+  console.log('f2 is', b);
+}
+
+console.log(c)
+
+f(a)
+f2(a)
+
+
+
