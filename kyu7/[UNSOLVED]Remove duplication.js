be8cf989f4f9768e09141de0a0298533ecddb77f
Vlad Danilov <danilovxp@gmail.com>
2022-01-11 20:50:23 -0800

    x

--- kyu7/[UNSOLVED]Remove duplication.js
+++
@@ -1,8 +0,0 @@
-// Remove duplication
-//Write a function that accepts an array of number as parameter, and it returns array after removing all duplicated numbers.
-function removeDuplication(arr) {
- return arr.indexOf(2)
-}
-console.log(removeDuplication([1, 2, 3, 2, 4, 6, 2, 6, 7])); //, [1,3,4,7]);
-//console.log(removeDuplication([1,2,1,2,1,1,2,2]))//, []);
-//console.log(removeDuplication([2,5,6,7,5,2,6]))//, [7]);
